b28afad8559154c08aa4b50bed11d496c1bbf6d4
Tom Wiesing <tkw01536@gmail.com>
2014-07-06 12:16:53 +0200

    Implemented PermissionNode.js

---
+++ backend/voting/PermissionNode.js
@@ -0,0 +1,151 @@
+
+/**
+ * Creates a new PermissionNode
+ * @method PermissionNode
+ * @param {object} [source_object] - Optional JSON source for the permissionNode.
+ */
+var PermissionNode = function(source_object){
+    this._data = [];
+
+    if(typeof source_object !== "undefined"){
+        this.fromJSON(source_object);
+    }
+}
+
+/**
+ * Updates a permissionNode from JSON.
+ * @param {object} source_object - JSON source for the permissionNode.
+ */
+PermissionNode.prototype.fromJSON = function(source_object){
+    this._data = source_object;
+    return this;
+}
+
+/**
+ * Returns the JSON source of this object.
+ */
+PermissionNode.prototype.toJSON = function(){
+    return this._data;
+}
+
+/**
+ * Checks if a user with the given data matches the given Rule.
+ * @param {object} user_data - JSON-style user information.
+ * @param {object} rule - Rule to match.
+ */
+PermissionNode.prototype.matchesRule = function(user_data, rule){
+    //get the two different values from the rule and the data
+
+    var value = user_data[rule["fieldName"]];
+    var query = rule["value"];
+
+    //in case we have an exception, this is our result
+    var result = rule["exceptionHandling"];
+
+    try{
+        switch(rule["fieldRelation"]){
+            //check equality
+            case "equals":
+                result = (value.toString() === query.toString());
+                break;
+            //check equality without case
+            case "equalsNoCase":
+                result = (value.toString().toLowerCase() === query.toString().toLowerCase());
+                break;
+            //check if one string contains another
+            case "contains":
+                result = (value.toString().indexOf(query.toString()) !== -1);
+                break;
+            //check if one string starts with the other
+            case "startsWith":
+                result = (value.toString().slice(0, query.toString().length) == query.toString());
+                break;
+            //check if one string ends with the other
+            case "endsWith":
+                result = (value.toString().indexOf(query.toString(), this.length - query.toString().length) !== -1);
+                break;
+            //check if a regular expression matches
+            case "matches":
+                result = (new RegExp(query.toString())).test(value.toString());
+                break;
+            //check if its bigger then as a number
+            case "biggerThen":
+                result = (parseFloat(value) > parseFloat(query));
+                break;
+            //check if its bigger then as a number
+            case "smallerThen":
+                result = (parseFloat(value) < parseFloat(query));
+                break;
+            default:
+                throw "UnknownComparator";
+        }
+
+        //negate if we need to
+        if(rule["negate"]){
+            result = !result;
+        }
+    } catch(e){
+        //TODO: Better logging
+        console.warn("PermissionNodeError: "+e);
+    }
+
+    return result;
+}
+
+/**
+ * Checks if a user with the given data matches the PermissionNode.
+ * @param {object} user_data - JSON-style user information.
+ */
+PermissionNode.prototype.matches = function(user_data){
+
+    var me = this;
+
+    var state = undefined;
+    var relation = "";
+
+    this._data.map(function(i, e){
+        if(i==0){
+            //initial check, we do not care about the relation
+            state = me.matchesRule(user_data, e);
+        } else {
+            //we now use the relation
+            //we always have state first, because JS is lazy in evaluating relations and we can use that (a lot).
+
+            if (relation == "or"){
+                state = state || me.matchesRule(user_data, e);
+            } else /* if(relation == "and") */{
+                state = state && me.matchesRule(user_data, e);
+            }
+
+        }
+
+        relation = e["nextRelation"];
+    })
+
+    return state;
+}
+
+/**
+ * Finds all users matching this permissionNode.
+ * @param {object} users - List of all users.
+ */
+PermissionNode.prototype.findMatchingUsers = function(users){
+    var me = this;
+    return users.filter(function(user){ return me.matches(user); });
+}
+
+
+module.exports = PermissionNode;
+
+/*
+    PermissionNode JSON:
+
+    {
+        "negate": true || false,
+        "fieldName": <NameOfFieldToCheck>,
+        "fieldRelation": "equals" || "equalsNoCase" || "contains" || "startsWith" || "endsWith" || "matches" || "biggerThen" || "smallerThen",
+        "value": <Value|RegEx>,
+        "nextRelation": "and" || "or",
+        "exceptionHandling": true || false
+    }
+*/
