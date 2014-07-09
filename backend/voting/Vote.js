5d98de6f0e266ba773628e97c12953b6ee5b8cad
Tom Wiesing <tkw01536@gmail.com>
2014-07-09 19:02:44 +0200

    Added vote class

---
+++ backend/voting/Vote.js
@@ -0,0 +1,46 @@
+/**
+ * Creates a new Vote.
+ *
+ * @constructor
+ * @this {Vote}
+ * @param {Vote.Source} [source_object] - Optional JSON-style source for the vote.
+ */
+var Vote = function(source_object){
+    this._data = [];
+
+    if(typeof source_object !== "undefined"){
+        this.fromJSON(source_object);
+    }
+}
+
+/**
+* Updates this Vote form JSON data.
+*
+* @param {Vote.Source} [source_object] - JSON-style source for this vote.
+*/
+Vote.prototype.fromJSON = function(source_object){
+    //TODO: Implement this.
+}
+
+
+/**
+ * Vote Stages.
+ *
+ * @enum {string}
+ */
+Vote.Stage = {
+    /** Vote has been created and is ready to be scheduled. */
+    INIT: 0,
+    /** Vote is open and people can vote. */
+    OPEN: 1,
+    /** Voting is closed.  */
+    CLOSED: 2,
+    /** Vote is closed and results are now public. */
+    PUBLIC: 3
+};
+
+/**
+ * A JSON-style source object of the vote. Intended to be used to store in a database.
+ *
+ * @typedef {Object} Vote.Source
+ */
